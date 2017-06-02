2b75a625365f9b4d35875247aa4ef0a26b28be94
zybooks <zybooks@zybookss-MacBook-Air.local>
2017-06-02 16:41:42 -0700

    43 style errors

--- app/library/route.js
+++ app/library/route.js
@@ -2,40 +2,36 @@ import Ember from 'ember';
 import ZyBookModel from '../models/zybook';
 
 export default Ember.Route.extend({
-  authentication: Ember.inject.service('session'),
-  beforeModel() {
-    if (!this.get('authentication.session.auth_token')) {
-      this.replaceWith('welcome');
+    authentication: Ember.inject.service('session'),
+    beforeModel() {
+        if (!this.get('authentication.session.auth_token')) {
+            this.replaceWith('welcome');
+        }
+    },
+    model() {
+        return new Ember.RSVP.Promise((resolve, reject) => {
+            Ember.$.ajax({
+                url: `https://zyserver-dev.zybooks.com/v1/user/${this.get('authentication.user.user_id')}/zybooks`,
+                method: "GET",
+                contentType: null,
+                data: { auth_token: this.get('authentication.session.auth_token') },
+                success: function(serverResponse) {
+                    let user_zybooks = [];
+                    serverResponse.zybooks.forEach((zybook) => {
+                        const zyBook = ZyBookModel.create({}).initialize(zybook);
+                        user_zybooks.push(zyBook);
+                    });
+                    if (serverResponse.success) {
+                        resolve(user_zybooks);
+                    }
+                    else {
+                        reject();
+                    }
+                },
+
+            });
+        })
+
     }
-  },
-  model() {
-    return new Ember.RSVP.Promise((resolve, reject) => {
-      Ember.$.ajax({
-        url: "https://zyserver-dev.zybooks.com/v1/user/" + this.get('authentication.user.user_id') + "/zybooks",
-        method: "GET",
-        contentType: null,
-        data: {"auth_token": this.get('authentication.session.auth_token')},
-        success: function(serverResponse) {
-          //console.log("SERVER RESPONSE:");
-          //console.log(serverResponse);
-          let user_zybooks = new Array();
-          serverResponse.zybooks.forEach((zybook) => {
-            //console.log(zybook);
-            const zyBook = ZyBookModel.create({}).initialize(zybook);
-            //console.log(zyBook);
-            user_zybooks.push(zyBook);
-          });
-          //console.log(user_zybooks);
-          if (serverResponse.success) {
-            resolve(user_zybooks); // Need to resolve a new array of models here
-          } else {
-            reject();
-          }
-        },
-
-      });
-    })
-
-  }
 
 });
