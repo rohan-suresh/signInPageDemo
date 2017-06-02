a40dc49ab0ce033149b5886d1c5a1d3290b8e293
zybooks <zybooks@zybookss-MacBook-Air.local>
2017-06-02 15:53:15 -0700

    142 style errors

--- app/table/route.js
+++ app/table/route.js
@@ -1,39 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  authentication: Ember.inject.service('session'),
-  beforeModel() {
-    if (!this.get('authentication.session.auth_token')) {
-      this.replaceWith('welcome');
-    }
-  },
-  model(params) {
-    return new Ember.RSVP.Promise((resolve, reject) => {
-      Ember.$.ajax({
-        url: "https://zyserver-dev.zybooks.com/v1/user/" + this.get('authentication.user.user_id') + "/zybooks",
-        method: "GET",
-        contentType: null,
-        data: {"auth_token": this.get('authentication.session.auth_token')},
-        success: function(serverResponse) {
-          //console.log("SERVER RESPONSE:");
-          //console.log(serverResponse);
-          let selected_zybook = null;
-          serverResponse.zybooks.forEach((zybook) => {
-             if (zybook.zybook_code == params.zybook_code) {
-               selected_zybook = zybook;
-             }
-          })
-          //console.log(selected_zybook);
-          if (serverResponse.success) {
-            resolve(selected_zybook);
-          } else {
-            reject();
-          }
-        },
+    authentication: Ember.inject.service('session'),
+    beforeModel() {
+        if (!this.get('authentication.session.auth_token')) {
+            this.replaceWith('welcome');
+        }
+    },
+    model(params) {
+        return new Ember.RSVP.Promise((resolve, reject) => {
+            Ember.$.ajax({
+                url: 'https://zyserver-dev.zybooks.com/v1/user/' + this.get('authentication.user.user_id') + '/zybooks',
+                method: 'GET',
+                contentType: null,
+                data: { 'auth_token': this.get('authentication.session.auth_token') },
+                success: function(serverResponse) {
+                    let selected_zybook = null;
 
-      });
-    })
+                    serverResponse.zybooks.forEach(zybook => {
+                        if (zybook.zybook_code === params.zybook_code) {
+                            selected_zybook = zybook;
+                        }
+                    });
+                    if (serverResponse.success) {
+                        resolve(selected_zybook);
+                    }
+                    else {
+                        reject();
+                    }
+                },
 
-  }
+            });
+        });
+
+    },
 
 });
